431790061be2ed73e157a3bf05fc6bde4ad8d28a
Anthony Sameishchev <anton.sameishchev@devexpress.com>
2022-06-03 12:47:04 +0400

    code added

---
+++ CS/winui_mvvm_custom_service/winui_mvvm_custom_service/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.WinUI.Core;
+using DevExpress.WinUI.Grid;
+using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winui_mvvm_custom_service
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window {
+        public ViewModel ViewModel { get; set; }
+        public MainWindow() {
+            this.InitializeComponent();
+            ViewModel = new ViewModel();
+        }
+    }
+    public interface IUpdateService {
+        void BeginUpdate();
+        void EndUpdate();
+    }
+    public class GridUpdateService : UIServiceBase<GridControl>, IUpdateService {
+        public void BeginUpdate() {
+            AssociatedObject?.BeginDataUpdate();
+        }
+        public void EndUpdate() {
+            AssociatedObject?.EndDataUpdate();
+        }
+    }
+}
---
+++ CS/winui_mvvm_custom_service/winui_mvvm_custom_service/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using DevExpress.Mvvm;
+using Microsoft.UI.Xaml;
+
+namespace winui_mvvm_custom_service {
+    public class ViewModel : ViewModelBase {
+        public ICommand LockUpdatesCommand { get; set; }
+        public ICommand UnlockUpdatesCommand { get; set; }
+        public ObservableCollection<Customer> Customers { get; set; }
+        public IUpdateService GridUpdateService => GetUIService<IUpdateService>();
+
+        DispatcherTimer updateTimer = new DispatcherTimer();
+        Random rnd = new Random();
+        public ViewModel() {
+            LockUpdatesCommand = new DelegateCommand(LockUpdates);
+            UnlockUpdatesCommand = new DelegateCommand(UnlockUpdates);
+            Customers = new ObservableCollection<Customer>();
+            for (int i = 1; i < 5; i++) {
+                Customers.Add(new Customer() { ID = i, Name = "Name" + i });
+            }
+            updateTimer.Interval = new TimeSpan(0, 0, 1);
+            updateTimer.Tick += UpdateRandomItem;
+            updateTimer.Start();
+        }
+
+        private void UpdateRandomItem(object sender, object e) {
+            Customers.Add(new Customer() { ID = Customers.Count, Name = "Name" + Customers.Count });
+        }
+        public void LockUpdates() { GridUpdateService.BeginUpdate(); }
+        public void UnlockUpdates() { GridUpdateService.EndUpdate(); }
+    }
+
+    public class Customer { 
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
